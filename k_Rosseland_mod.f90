6a4ec28b1e861d096e4a85107206110b5fc17383
Graham KH Lee <38210132+GKH-Lee@users.noreply.github.com>
2020-10-09 14:41:42 +0200

    Update k_Rosseland_mod.f90

--- k_Rosseland_mod.f90
+++ k_Rosseland_mod.f90
@@ -13,9 +13,7 @@ module k_Rosseland_mod
   use, intrinsic :: iso_fortran_env
   implicit none
   !! Precision variables
-  integer, parameter :: sp = REAL32
   integer, parameter :: dp = REAL64
-  integer, parameter :: qp = REAL128
 
   real(kind=dp), parameter :: pi = 4.0_dp * atan(1.0_dp)
   real(kind=dp), parameter :: onedivpi = 1.0_dp/pi
@@ -65,22 +63,25 @@ contains
 
   !! Calculates the IR band Rosseland mean opacity (local T) according to the
   !! Freedman et al. (2014) fit and coefficents
-  subroutine k_Ross_Freedman(T, P, met, k_IR)
+  subroutine k_Ross_Freedman(Tin, Pin, met, k_IR)
     implicit none
 
     ! Input:
     ! T - Local gas temperature [K]
-    ! P - Local gas pressure [dyne cm-2]
+    ! P - Local gas pressure [pa]
     ! met - Local metallicity [M/H] (log10 from solar, solar [M/H] = 0.0)
 
     ! Output:
-    ! k_IR - IR band Rosseland mean opacity [cm2 g-1]
+    ! k_IR - IR band Rosseland mean opacity [m2 kg-1]
 
-    real(kind=dp), intent(in) :: T, P, met
+    real(kind=dp), intent(in) :: Tin, Pin, met
     real(kind=dp), intent(out) :: k_IR
 
     real(kind=dp) :: k_lowP, k_hiP
     real(kind=dp) :: Tl10, Pl10
+
+    T = Tin
+    P = Pin * 10.0_dp ! Convert to dyne cm-2
 
     Tl10 = log10(T)
     Pl10 = log10(P)
@@ -107,24 +108,20 @@ contains
     ! De log10
     k_hiP = 10.0_dp**k_hiP
 
-    ! Total Rosseland mean opacity
-    k_IR = k_lowP + k_hiP
+    ! Total Rosseland mean opacity - converted to m2 kg-1
+    k_IR = (k_lowP + k_hiP) /10.0_dp
 
   end subroutine k_Ross_Freedman
 
   !! Calculates 3 band grey visual gamma values and 2 picket fence IR gamma values
   !! according to the coefficents and equations in:
   !! Parmentier & Menou (2014) and Parmentier et al. (2015)
-  !! NOTE: noon_longitude = sub-stellar point longitude
   !! NOTE: This does not calculate the opacity - call k_Ross_Freedman for that
-  subroutine gam_Parmentier(Tint, Tirr, table_num, lat, lon, gam_V, Beta_V,  Beta, gam_1, gam_2)
+  subroutine gam_Parmentier(Teff, table_num, gam_V, Beta_V, Beta, gam_1, gam_2, gam_P, tau_lim)
     implicit none
 
     ! Input:
-    ! Tint - Internal temperature [K]
-    ! Tirr - Sub-stellar irradiation temperature [K]
-    ! lat - latitude of profile [rad]
-    ! lon - longitude of profile [rad]
+    ! Teff - Effective temperature [K]
     ! table_num - Table selection from Parmentier et al. (2015): 1 = w. TiO/VO, 2 = w.o. TiO/VO
 
     ! Output:
@@ -133,35 +130,21 @@ contains
     ! Beta - equilvalent bandwidth for picket fence IR model
     ! gam_1 - gamma ratio for IR band 1 (gam_1 = kIR_1/kIR_Ross)
     ! gam_2 - gamma ratio for IR band 2 (gam_2 = kIR_2/kIR_Ross)
+    ! gam_P - gamma ratio for Planck mean (gam_P = kP/kRoss)
+    ! tau_lim - tau limit variable (usually for IC system)
 
-    real(kind=dp), intent(in) :: Tint, Tirr, lat, lon
+    real(kind=dp), intent(in) :: Teff
     integer, intent(in) :: table_num
 
     real(kind=dp), dimension(3), intent(out) :: gam_V, Beta_V
     real(kind=dp), dimension(2), intent(out) :: Beta
-    real(kind=dp), intent(out) :: gam_1, gam_2
+    real(kind=dp), intent(out) :: gam_1, gam_2, gam_P, tau_lim
 
-    real(kind=dp) :: R, gam_P, Teff
+    real(kind=dp) :: R
     real(kind=dp) :: aP, bP, cP
     real(kind=dp) :: aV1, bV1, aV2, bV2, aV3, bV3
     real(kind=dp) :: aB, bB, AB_res
-    real(kind=dp) :: l10T, l10T2, RT, cosangle
-    real(kind=dp), parameter :: noon_longitude = 0.0_dp
-
-    ! Calculate Teff for this profile through:
-    ! Teff**4 = mu * Tirr**4 + Tint**4
-    ! mu = cosangle from substellar point
-    cosangle = cos(lat)*cos(lon - noon_longitude*deg_to_rad)
-
-    if (cosangle <= 0.0) then
-      ! Nightside profiles Teff = Tint
-      Teff = Tint**4
-    else
-      ! Dayside profiles
-      Teff = cosangle * Tirr**4 + Tint**4
-    end if
-    ! Quad root here
-    Teff = Teff**(0.25_dp)
+    real(kind=dp) :: l10T, l10T2, RT
 
     ! Log 10 T_eff variables
     l10T = log10(Teff)
@@ -267,8 +250,8 @@ contains
 
     ! gamma_Planck - if < 1 then make it grey approximation (k_Planck = k_Ross, gam_P = 1)
     gam_P = 10.0_dp**(aP * l10T2 + bP * l10T + cP)
-    if (gam_P < 1.0_dp) then
-      gam_P = 1.0_dp
+    if (gam_P < 1.0000001_dp) then
+      gam_P = 1.0000001_dp
     end if
 
     ! equivalent bandwidth value
@@ -283,41 +266,44 @@ contains
     gam_1 = Beta(1) + R - Beta(1)*R
     gam_2 = gam_1 / R
 
+    ! Calculate tau_lim parameter
+    tau_lim = 1.0_dp/(gam_1*gam_2) * sqrt(gam_P/3.0_dp)
+
   end subroutine gam_Parmentier
 
   ! Calculates the Bond Albedo according to Parmentier et al. (2015) expression
-  subroutine Bond_Parmentier(T_eff, grav,  AB)
+  subroutine Bond_Parmentier(Teff0, grav,  AB)
     implicit none
 
     ! Input:
-    ! Teff - Atmospheric profile effective temperature [K] (See gam_Parmentier)
+    ! Teff0 - Atmospheric profile effective temperature [K] with zero albedo
     ! grav - Surface gravity of planet [m s-2]
 
     ! Output:
     ! AB - Bond albedo
 
-    real(kind=dp), intent(in) :: T_eff, grav
+    real(kind=dp), intent(in) :: Teff0, grav
     real(kind=dp), intent(out) :: AB
 
     real(kind=dp) :: a, b
 
     ! a and b cofficents dependent on T_eff and grav
-    if (T_eff <= 250.0_dp) then
+    if (Teff0 <= 250.0_dp) then
       a = -0.335_dp * grav**(0.070_dp)
       b = 0.0_dp
-    else if ((T_eff > 250.0_dp) .and. (T_eff <= 750.0_dp)) then
+    else if ((Teff0 > 250.0_dp) .and. (Teff0 <= 750.0_dp)) then
       a = -0.335_dp * grav**(0.070_dp) + 2.149_dp * grav**(0.135_dp)
       b = -0.896_dp * grav**(0.135_dp)
-    else if ((T_eff > 750.0_dp) .and. (T_eff < 1250.0_dp)) then
+    else if ((Teff0 > 750.0_dp) .and. (Teff0 < 1250.0_dp)) then
       a = -0.335_dp * grav**(0.070_dp) -  0.428_dp * grav**(0.135_dp)
       b = 0.0_dp
-    else if (T_eff >= 1250.0_dp) then
+    else if (Teff0 >= 1250.0_dp) then
       a = 16.947_dp - 3.174_dp * grav**(0.070_dp) - 4.051_dp * grav**(0.135_dp)
       b = -5.472_dp + 0.917_dp * grav**(0.070_dp) + 1.170_dp * grav**(0.135_dp)
     end if
 
     ! Final Bond Albedo expression
-    AB = 10.0_dp**(a + b * log10(T_eff))
+    AB = 10.0_dp**(a + b * log10(Teff0))
 
   end subroutine Bond_Parmentier
 
